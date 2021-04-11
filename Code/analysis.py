6d5d2e977618742da143f5fcc540e53d60b79b8d
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2021-04-11 20:28:26 +0100

    using pandas dataframes for iris set data

---
+++ Code/analysis.py
@@ -0,0 +1,19 @@
+# analysis.py
+# The purpose of this program is to carry out analysis on the Iris Data Set and output the findings.
+# author: Emma Farrell
+
+# When this gets run, the analysis gets carried out and outputted to a file + images
+# Summary data - mean, std deviation, mode etc.
+# Plots, histograms, scatter plots, bar charts
+# file type: csv
+# need to pivot? 
+# df.head() to print out the first few lines to give idea of the content
+
+import pandas as pd 
+
+column_names = ['Sepal Length (cm)', 'Sepal Width (cm)', 'Petal Length (cm)', 'Petal Width (cm)', 'Class']
+
+iris_data_frame = pd.read_csv("../irisdata.csv", names = column_names)
+#iris_data_frame.columns = 
+
+print(iris_data_frame)
