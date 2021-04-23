2ab7e5c5aa0553413e9ad2d681cf31b0cc4be887
ByketPoe <32248836+ByketPoe@users.noreply.github.com>
2021-04-24 00:42:53 +0100

    dividing data by columns

--- Code/analysis.py
+++ Code/analysis.py
@@ -15,18 +15,28 @@ import seaborn as sb
 
 # Split classes into different files
 
-column_names = ['Sepal Length (cm)', 'Sepal Width (cm)', 'Petal Length (cm)', 'Petal Width (cm)', 'Class']
+column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
 
 iris_data_frame = pd.read_csv("../irisdata.csv", names = column_names)
-setosa_df = iris_data_frame[iris_data_frame['Class'] == 'Iris-setosa']
-versicolor_df = iris_data_frame[iris_data_frame['Class'] == 'Iris-versicolor']
-virginica_df = iris_data_frame[iris_data_frame['Class'] == 'Iris-virginica']
+
+setosa_df = iris_data_frame[iris_data_frame['class'] == 'Iris-setosa']
+versicolor_df = iris_data_frame[iris_data_frame['class'] == 'Iris-versicolor']
+virginica_df = iris_data_frame[iris_data_frame['class'] == 'Iris-virginica']
+
+s_length = iris_data_frame['sepal_length']
+s_width = iris_data_frame['sepal_width']
+p_length = iris_data_frame['petal_length']
+p_width = iris_data_frame['petal_width']
 #iris_data_frame.columns = 
 sb.set_theme()
-sb.histplot(iris_data_frame['Sepal Length (cm)'])
+sb.histplot(iris_data_frame['sepal_length'])
 plt.show()
 
-print(iris_data_frame.describe())
-print(setosa_df.describe())
-print(versicolor_df.describe())
-print(virginica_df.describe())
+#print(iris_data_frame.describe())
+#print(setosa_df.describe())
+#print(versicolor_df.describe())
+#print(virginica_df.describe())
+#print(s_length)
+#print(s_width)
+#rint(p_length)
+print(p_width.describe())
